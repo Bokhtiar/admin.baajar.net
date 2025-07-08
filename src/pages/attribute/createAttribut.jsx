c7742fd290e9894ef313a11b7cef4eea3d73260e
mahmudul <mahmudul35-218@diu.edu.bd>
2025-07-08 11:48:12 +0600

    button

--- src/pages/attribute/createAttribut.jsx
+++ src/pages/attribute/createAttribut.jsx
@@ -114,7 +114,7 @@ export default function CreateAttributeModal({ onClose, fetchAttribute }) {
               className="appearance-none w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none text-gray-500 pr-8 cursor-pointer "
               defaultValue="active"
             >
-              <option value="0">Select Attribute</option>
+              <option value="0">Select Unit</option>
               {data &&
                 data.map((item) => (
                   <option key={item.id} value={item.id}>
